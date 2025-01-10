051be7b75ff55b13afd6550d3418eff4da3cce56
Minaazzcod <minaazzcod@gmail.com>
2025-01-10 15:43:28 +0900

    초기화

--- index.js
+++
@@ -1,5 +0,0 @@
-const selectElement = document.getElementById("color");
-const resultElement = document.getElementById("result");
-selectElement.addEventListener("change",()=>{
-	resultElement.textContent = selectElement.value
-})
