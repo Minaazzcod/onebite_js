b5155a34c6e21ddad2f54aa5a319a51a789a46dd
MinaKim <minaazzcod@gmail.com>
2025-03-26 21:48:11 +0900

    Day2 mission

---
+++ day2/index.js
@@ -0,0 +1,19 @@
+let animal = 'cat';
+
+switch (animal) {
+	case 'dog' :
+		console.log('🐶강아지🐶');
+		break;
+	case 'tiger' :
+		console.log('🐯호랑이🐯');
+		break;
+	case 'cat' :
+		console.log('🐱고양이🐱');
+		break;
+	case 'lion' :
+		console.log('🦁사자🦁');
+		break;
+	default: 
+		console.log('❌일치하는 동물이 없습니다❌')
+
+} 
