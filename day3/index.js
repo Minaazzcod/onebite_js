72acad2f9ad836fc7a88b96dbbdf56a0d3009bbd
MinaKim <minaazzcod@gmail.com>
2025-03-26 22:10:00 +0900

    Day3 Mission

--- day3/index.js
+++ day3/index.js
@@ -1,12 +1,10 @@
 // Day3 Mission01
-
 const multFunction = (num1,num2) => num1*num2;
 
 console.log(multFunction(3,4));
 
 
 // Day3 Mission02
-
 const bookInfo = {
 	title : '소년이 온다',
 	anthor : '한강',
@@ -19,7 +17,6 @@ console.log(bookInfo.getSummary());
 
 
 // Day3 Mission03
-
 function sumNumbers(a,b) {
 	function addFive(number){
 		return number + 5;
